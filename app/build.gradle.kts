772461cf480c0ae8f85a235b1edc7daf3fd31468
Peter Okafor <peter.obinna.okafor@gmail.com>
2026-03-26 13:44:54 +0100

    chore: Bump compileSdk and targetSdk to 35 for Play Store compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,12 +16,12 @@ val keystoreProperties = Properties().apply {
 
 android {
     namespace = "com.musicplayer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.niftyfissions.musicplayer"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
